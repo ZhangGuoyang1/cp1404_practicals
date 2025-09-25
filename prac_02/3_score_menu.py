b82bf9f820d5e7c3ae511fb18835780cc873d7b0
guoyang <guoyang.zhang@my.jcu.edu.au>
2025-09-25 19:44:30 +0800

    modify

--- prac_02/3_score_menu.py
+++ prac_02/3_score_menu.py
@@ -42,25 +42,7 @@ def show_stars(score):
     print('*' * int(score))
 
 
-def main():
-    score = get_valid_score()
-    print(f"Your initial score: {score:.2f} -> {get_score_result(score)}")
-    MENU = "1. (G)et a valid score \n2. (P)rint result \n3. (S)how stars \n4. (Q)uit"
-    print(MENU)
-    choice = input("Get choice:")
-    while choice != 'Q':
-        if choice == 'G':
-            score = get_valid_score()
-            print(f"Your score: {score:.2f} -> {get_score_result(score)}")
-        elif choice == 'P':
-            print(f"Score result: {get_score_result(score)}")
-        elif choice == 'S':
-            show_stars(score)
-        else:
-            print("Invalid choice, please try again.")
-        print(MENU)
-        choice = input("Get choice:")
-    print(f"Farewell! Your final score was: {score:.2f}")
+
 
 
 main()
