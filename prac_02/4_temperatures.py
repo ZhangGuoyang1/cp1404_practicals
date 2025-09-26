6fc1a49714a857d9fb5f0e8f77c6da2eb765516b
guoyang <guoyang.zhang@my.jcu.edu.au>
2025-09-26 12:32:13 +0800

    Add comments and put the main function in front.

--- prac_02/4_temperatures.py
+++ prac_02/4_temperatures.py
@@ -1,29 +1,66 @@
-def celsius_to_fahrenheit(celsius):
-    return celsius * 9.0 / 5 + 32
+import random
+
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+EXCELLENT_THRESHOLD = 90
+PASS_THRESHOLD = 50
+
+MENU = "(G)et a valid score \n(P)rint result \n(S)how stars \n(Q)uit"
 
 
-def fahrenheit_to_celsius(fahrenheit):
-    return 5 / 9 * (fahrenheit - 32)
+def main():
+    """Main function with menu loop."""
+    student_score = -1
 
-
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius_to_fahrenheit(celsius)
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = fahrenheit_to_celsius(fahrenheit)
-        print(f"Result: {celsius:.2f} C")
-    else:
-        print("Invalid option")
     print(MENU)
-    choice = input(">>> ").upper()
+    choice = input("Enter choice: ").upper()
+    while choice != 'Q':
+        if choice == 'G':
+            student_score = get_score()
+        elif choice == 'P':
+            student_score = validate_score(student_score)
+            print(get_grade(student_score))
+        elif choice == 'S':
+            student_score = validate_score(student_score)
+            display_stars(student_score)
+        else:
+            print("Invalid choice, please try again.")
+        print(MENU)
+        choice = input("Enter choice:").upper()
+    print("Farewell!")
 
-print("Thank you.")
+
+def validate_score(student_score):
+    if student_score == -1:
+        student_score = get_score()
+    return student_score
+
+
+def display_stars(student_score):
+    print(int(student_score) * "*")
+
+
+def get_valid_score():
+    student_score = float(input("Enter score: "))
+    while student_score < MINIMUM_SCORE or student_score > MAXIMUM_SCORE:
+        print("invalid score")
+        student_score = float(input("Enter score: "))
+    return student_score
+
+
+def get_score():
+    """Helper to get a score if not set."""
+    return get_valid_score()
+
+
+def get_grade(score):
+    if score >= EXCELLENT_THRESHOLD:
+        message = "Excellent"
+    elif score >= PASS_THRESHOLD:
+        message = "Pass"
+    else:
+        message = "Bad"
+    return message
+
+
+main()
