b31ee88ff369c702ef1c5b06e2e5e9778b069d08
guoyang <guoyang.zhang@my.jcu.edu.au>
2025-09-26 12:23:33 +0800

    Put the main function at the front

--- prac_02/1_password_stars.py
+++ prac_02/1_password_stars.py
@@ -1,21 +1,19 @@
-def get_password(min_length):
-    password = input("Enter your password: ")
-
-    while len(password) < min_length:
-        print(f"Password must be at least {min_length} characters long.")
-        password = input("Enter your password: ")
-
-    return password
-
-
-def print_asterisks(password):
-    print("*" * len(password))
+PASSWORD_LENGTH = 8
 
 
 def main():
-    min_length = 8
-    password = get_password(min_length)
-    print_asterisks(password)
+    """get password and print stars"""
+    password = validate_password()
+    print(len(password) * "*")
+
+
+def validate_password():
+    """Ask for a valid password"""
+    password = input("Enter your password: ")
+    while len(password) < PASSWORD_LENGTH:
+        print("Invalid password")
+        password = input("Enter your password: ")
+    return password
 
 
 main()
