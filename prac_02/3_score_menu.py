4d972edfcf1f317fea633b56756986bf38af566b
guoyang <guoyang.zhang@my.jcu.edu.au>
2025-09-26 12:30:41 +0800

    Add comments and put the main function in front.

--- prac_02/3_score_menu.py
+++ prac_02/3_score_menu.py
@@ -1,48 +1,36 @@
+import random
+
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+EXCELLENT_THRESHOLD = 90
+PASS_THRESHOLD = 50
+
+
 def main():
-    score = get_valid_score()
-    print(f"Your initial score: {score:.2f} -> {get_score_result(score)}")
-    MENU = "1. (G)et a valid score \n2. (P)rint result \n3. (S)how stars \n4. (Q)uit"
-    print(MENU)
-    choice = input("Get choice:")
-    while choice != 'Q':
-        if choice == 'G':
-            score = get_valid_score()
-            print(f"Your score: {score:.2f} -> {get_score_result(score)}")
-        elif choice == 'P':
-            print(f"Score result: {get_score_result(score)}")
-        elif choice == 'S':
-            show_stars(score)
-        else:
-            print("Invalid choice, please try again.")
-        print(MENU)
-        choice = input("Get choice:")
-    print(f"Farewell! Your final score was: {score:.2f}")
-    
-
-def get_valid_score():
-    score = float(input("Enter score (0-100): "))
-    while score < 0 or score > 100:
-        print("Invalid score. Please enter a score between 0 and 100.")
-        score = float(input("Enter score (0-100): "))
-    return score
+    score = get_score()
+    grade = get_grade(score)
+    print(grade)
+    random_number = random.uniform(MINIMUM_SCORE, MAXIMUM_SCORE)
+    random_grade = get_grade(random_number)
+    print(random_grade)
 
 
-def get_score_result(score):
-    if score < 0 or score > 100:
-        return "Invalid score"
-    elif score >= 90:
-        return "Excellent"
-    elif score >= 50:
-        return "Passable"
+def get_grade(score):
+    if score >= EXCELLENT_THRESHOLD:
+        message = "Excellent"
+    elif score >= PASS_THRESHOLD:
+        message = "Pass"
     else:
-        return "Bad"
+        message = "Bad"
+    return message
 
 
-def show_stars(score):
-    print('*' * int(score))
-
-
-
+def get_score():
+    score = float(input("Enter score: "))
+    while score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    return score
 
 
 main()
