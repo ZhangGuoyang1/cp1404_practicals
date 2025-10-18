1321949bd35b437ec638ae6fa3634eb3fb2813da
guoyang <guoyang.zhang@my.jcu.edu.au>
2025-10-18 20:27:38 +0800

    Fix email-to-name program logic

---
+++ prac_05/4_emails.py
@@ -0,0 +1,42 @@
+def main():
+    """Create dictionary of emails-to-names."""
+    SPECIAL_CHARACTER = "@"
+    email_to_name = {}
+
+    email = input("Email: ")
+    while email != "":
+        while SPECIAL_CHARACTER not in email:
+            print("Error")
+            email = input("Email: ")
+
+        name = get_name(email)
+        confirmation = input(f"Is your name {name}? (Y/n): ").strip().upper()
+        if confirmation != "" and confirmation != "Y":
+            name = input("Name: ")
+
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    print_email(email_to_name)
+
+
+def print_email(email_to_name):
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def get_name(email):
+    """Extract expected name from email address."""
+    prefix = email.split("@")[0]
+    parts = prefix.split(".")
+    name = " ".join(parts).title()
+    return name
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
