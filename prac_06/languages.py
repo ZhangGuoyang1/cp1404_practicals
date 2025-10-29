2e269af2480158672349235ac6e21156d1211d8a
guoyang <guoyang.zhang@my.jcu.edu.au>
2025-10-29 16:08:34 +0800

    Implement Car and ProgrammingLanguage classes with demo scripts

---
+++ prac_06/languages.py
@@ -0,0 +1,17 @@
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+    print(visual_basic)
+    print(ruby)
+
+    languages = [python, visual_basic, ruby]
+    print("The dynamically typed languages are:")
+    dynamic_language = [language.name for language in languages if language.is_dynamic()]
+    print(dynamic_language)
+    for language in dynamic_language:
+        print(language)
