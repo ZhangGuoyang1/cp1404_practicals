b1afac8713c68ab48ca3ea12894c782ab18d1a6d
guoyang <guoyang.zhang@my.jcu.edu.au>
2025-10-31 13:32:14 +0800

    Add comments on the main function

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -2,6 +2,7 @@ from prac_06.programming_language import ProgrammingLanguage
 
 
 def main():
+    """Demonstrate the use of the ProgrammingLanguage class."""
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
@@ -10,6 +11,7 @@ def main():
     print(ruby)
 
     languages = [python, visual_basic, ruby]
+    # Display only the dynamically typed languages
     print("The dynamically typed languages are:")
     dynamic_language = [language.name for language in languages if language.is_dynamic()]
     print(dynamic_language)
