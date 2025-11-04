9cdf9d9d595980936d09278a0e49eca4e3cb0566
guoyang <guoyang.zhang@my.jcu.edu.au>
2025-11-04 17:32:03 +0800

    Fix date format handling, priority comparison, and string formatting in Project class.

--- prac_07/project.py
+++ prac_07/project.py
@@ -6,36 +6,36 @@ FULL_COMPLETION = 100
 class Project:
     def __init__(self, name="", start_date="", priority=int, cost=float, completion=int):
         self.name = name
-        self.start_date = datetime.datetime.strptime(start_date, "%d-%m-%Y").date()
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = priority
         self.cost = cost
         self.completion = completion
 
-   def __str__(self):
-       """Return string representation of Project object"""
-       return f"{self.name} {self.start_date}, {self.priority}, {self.completion}"
+    def __str__(self):
+        """Return string representation of Project object"""
+        return f"{self.name}, {self.start_date}, {self.priority}, {self.completion}"
 
-   def __repr__(self):
-       """Return string representation of Project object"""
-       return f"{self.name} {self.start_date}, {self.priority}, {self.completion}"
+    def __repr__(self):
+        """Return string representation of Project object"""
+        return f"{self.name} {self.start_date}, {self.priority}, {self.completion}"
 
-   def is_complete(self):
-       """Verify if Project object is complete"""
-       return int(self.completion) == FULL_COMPLETION
+    def is_complete(self):
+        """Verify if Project object is complete"""
+        return int(self.completion) == FULL_COMPLETION
 
-   def __lt__(self, other):
-       """Less than, used for sorting project"""
-       return self.priority <= other.priority
+    def __lt__(self, other):
+        """Less than, used for sorting project"""
+        return self.priority < other.priority
 
-   def compare_date(self, input_other):
-       """Compare the user_input date with project start date."""
-       input_date = datetime.datetime.strptime(input_other, "%d-%m-%Y").date()
-       return self.start_date >= input_date
+    def compare_date(self, input_other):
+        """Compare the user_input date with project start date."""
+        input_date = datetime.datetime.strptime(input_other, "%d-%m-%Y").date()
+        return self.start_date >= input_date
 
-   def update_percentage(self, value):
-       """Update the project completion percentage."""
-       self.completion = value
+    def update_percentage(self, value):
+        """Update the project completion percentage."""
+        self.completion = value
 
-   def update_priority(self, value):
-       """Update the project priority."""
-       self.priority = value
+    def update_priority(self, value):
+        """Update the project priority."""
+        self.priority = value
