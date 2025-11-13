1c4cdc927560431a14fdcb27f988533d8ebae794
guoyang <guoyang.zhang@my.jcu.edu.au>
2025-11-13 19:32:52 +0800

    Modify comments.

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -1,9 +1,3 @@
-"""
-CP1404/CP5632 Practical
-Kivy GUI program to square a number
-Lindsay Ward, IT@JCU
-"""
-
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
